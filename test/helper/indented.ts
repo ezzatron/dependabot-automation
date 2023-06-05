ec16e942e4ef8003e6acd226e71d178cd884e7ea
Erin Millard <contact@erin.id.au>
2023-06-05 23:04:01 +1000

    Support substitutions in the indented test helper

--- test/helper/indented.ts
+++ test/helper/indented.ts
@@ -7,14 +7,19 @@
  * beginning of a line in addition to what’s before the closing backtick, that
  * whitespace is included.
  */
-export function indented([string]: TemplateStringsArray): string {
-  const lines = string.split("\n");
-  const firstLine = lines[0];
-  const lastLine = lines[lines.length - 1];
+export function indented(
+  strings: TemplateStringsArray,
+  ...args: unknown[]
+): string {
+  const allStrings = strings.join("");
+  const stringsLines = allStrings.split("\n");
+  const firstLine = stringsLines[0];
+  const lastLine = stringsLines[stringsLines.length - 1];
 
   if (firstLine !== "") {
     throw new Error("The first line of an indented string must be empty.");
   }
+
   if (lastLine.trim() !== "") {
     throw new Error(
       "The last line of an indented string may only contain whitespace."
@@ -22,9 +27,31 @@ export function indented([string]: TemplateStringsArray): string {
   }
 
   const indent = lastLine.match(/^\s*/)?.[0] ?? "";
+  const indentPattern = new RegExp(`^${indent}`);
+  const allIndentPattern = new RegExp(`^${indent}`, "gm");
+  const contentLines = stringsLines.slice(1, -1);
 
-  return lines
-    .slice(1, -1)
-    .map((line) => line.replace(new RegExp(`^${indent}`), ""))
-    .join("\n");
+  for (let i = 0; i < contentLines.length; ++i) {
+    const line = contentLines[i];
+
+    if (line === "") continue;
+
+    if (!indentPattern.test(line)) {
+      throw new Error(
+        `Line ${i + 1} of indented string has incorrect indentation. ` +
+          `Line content: ${JSON.stringify(line)}}`
+      );
+    }
+  }
+
+  let composed = "";
+
+  for (let i = 0; i < strings.length; ++i) {
+    composed += strings[i].replaceAll(allIndentPattern, "");
+    if (i < args.length) composed += args[i];
+  }
+
+  const composedLines = composed.split("\n");
+
+  return composedLines.slice(1, -1).join("\n");
 }
